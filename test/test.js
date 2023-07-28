ede884e2425eb9cffa010364216b7cf949dbdd4f
Olivier Guiter <olivier.guiter@ioterop.com>
2023-07-28 15:49:19 +0000

    b

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ const puppeteer = require('puppeteer');
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('https://news.ycombinator.com', {
+  await page.goto('https://ioterop.github.io/IOWA', {
     waitUntil: 'networkidle2',
   });
   await page.pdf({ path: 'test.pdf', format: 'a4' });
